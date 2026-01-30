572ad25c650c31b37e56c849a1f3087f65685d65
Man1sh-Prasad <prasadmanish467@gmail.com>
2026-01-31 00:33:53 +0530

    refactor: rewrite logic for signup code integrating otp verification code

--- services/auth-service.ts
+++ services/auth-service.ts
@@ -1,14 +1,25 @@
 import api from "@/lib/axios";
 
-export const signup = async (data: {
+interface SignupData {
     firstname: string,
     lastname: string,
     email: string, 
     password: string
-}) => {
-    const response = await api.post('/api/v1/user/signup', data);
+}
+
+export const signup = async (data: SignupData, verificationToken: string) => {
+    const response = await api.post(
+        '/api/v1/user/signup',
+        data,
+        {
+            headers: {
+                "x-verification-token": verificationToken
+            }
+        }
+    );
     return response.data;
 }
+
 
 export const signin = async (data: {
     email: string, 
